26c2b314769679de29979f461fb545b36b98e2b1
Praveen Kumar <hawkpraveen33@gmail.com>
2024-07-25 18:56:59 +0530

    css updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
+        <div className='min-h-screen flex justify-center items-center'>
             <h1>About page</h1>
         </div>
     );
